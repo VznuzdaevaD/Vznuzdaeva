eb8be78985d387dc0c652490154ef056072b13b7
VznuzdaevaD <149108332+VznuzdaevaD@users.noreply.github.com>
2024-02-11 18:23:50 +0300

    Add files via upload

---
+++ Лабораторная работа 3/task_1.py
@@ -0,0 +1,46 @@
+class Book:
+    """ Базовый класс книги. """
+    def __init__(self, name: str, author: str):
+        self.name = name
+        self.author = author
+
+    @property
+    def name(self):
+        return self.name
+
+    @property
+    def author(self):
+        return self.author
+
+    def __str__(self):
+        return f"Книга {self.name}. Автор {self.author}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
+
+
+class PaperBook(Book):
+    def __init__(self, name: str, author: str, pages: int):
+        super().__init__(name, author)
+
+        if not isinstance(pages, int):
+            raise TypeError("Количество страниц должно быть типа int")
+        if pages <= 0:
+            raise ValueError("Количество страниц должно быть положительным числом")
+        self.pages = pages
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
+
+
+class AudioBook(Book):
+    def __init__(self, name: str, author: str, duration: float):
+        super().__init__(name, author)
+        if not isinstance(duration, float):
+            raise TypeError("Длительность книги должна быть типа float")
+        if duration <= 0:
+            raise ValueError("Длительность должна быть положительным числом")
+        self.duration = duration
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
